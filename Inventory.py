d431f1e472f505bb97423d348b00c250907ce124
Tej Bahadur Sahu <45288657+TejSahu@users.noreply.github.com>
2020-01-30 20:23:23 +0530

    # 2 corrected

--- Inventory.py
+++ Inventory.py
@@ -1,6 +1,10 @@
+#  ----------------items player currently has----------
 items = {'Arrow': 12, 'Bow': 1, 'Rifle': 2, 'Pistol': 5, 'Gold coin': 42}
 
-dragonLoot = ['Gold coin', 'Bow', 'Gold coin', 'Pistol', 'Arrow']
+#  ----------------items found in the loot stash------------
+dragonLoot = ['Gold coin', 'Bow', 'Gold coin', 'Pistol', 'Arrow', 'Bomb', 'Arrow', 'Pistol']
+
+#  -------------------fucntion to traverse through the item and display total item-------
 
 
 def displayinventory(inventory):
@@ -9,20 +13,22 @@ def displayinventory(inventory):
     for k, v in inventory.items():  # to display both key and values
         print(k + ' : ' + str(v))
         item_total += v
-    print("Total number of items: " + str(item_total))
+    print("Total number of items now: " + str(item_total))
+
+#  -----------fucntion to add items in the loot stash to items----------------
 
 
 def addtoinventory(inventory, more_items):
-    for k, v in inventory.items():
-        if k in more_items:
-            inventory[k] += 1
+    for i in more_items:
+        if i in inventory.keys():
+            inventory[i] += 1
         else:
-            inventory.setdefault(k, 1)
+            inventory.setdefault(i, 1)
 
     return inventory
 
 
-items = addtoinventory(items, dragonLoot)
-displayinventory(items)
-print(items)
+addtoinventory(items, dragonLoot)  # ---------updating items
+displayinventory(items)  # ---------call to fucntion to display the inventory
+
 # displayinventory(items)
